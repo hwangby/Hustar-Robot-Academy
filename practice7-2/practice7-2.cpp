41646021c4c75feba98a3bfd327b179069bacf60
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-07 17:37:40 +0900

    Add files via upload

---
+++ practice7-2/practice7-2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numbers[10];
+	cout << "Enter ten numbers: ";
+	for (int i = 0; i < 10; i++) {
+		cin >> numbers[i];
+	}
+	cout << "The reversed numbers: ";
+	for (int i = 0; i < 10; i++) {
+		cout << numbers[9 - i] << " ";
+	}
+}
