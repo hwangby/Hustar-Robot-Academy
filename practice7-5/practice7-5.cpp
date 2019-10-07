462a9717d7f69d9c4107126c5ffdbd6c8d7467c2
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-07 17:38:56 +0900

    Add files via upload

---
+++ practice7-5/practice7-5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numbers[10] = {0};
+	bool check = true;
+	
+	cout << "Enter ten numbers: ";
+	for (int i = 0; i < 10; i++) {
+		cin >> numbers[i];
+	}
+	cout << "The distinct numbers are : ";
+	for (int i = 0; i < 10; i++) {
+		for (int j = i-1; j >= 0; j--) {
+			if (numbers[j] == numbers[i]) {
+				check = false;
+				break;
+			}
+		}
+		if (check) {
+			cout << numbers[i] << " ";
+		}
+		else {
+			check = true;
+			continue;
+		}
+	}
+
+	return 0;
+}
