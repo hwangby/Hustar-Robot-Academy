b0906cb090bbb03a56b79272a55560201179fdf2
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-07 17:39:54 +0900

    Add files via upload

---
+++ practice7-7/practice7-7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand((unsigned int)time(0));
+	int counts[10] = { 0 };
+	int num;
+	for (int i = 0; i < 100; i++) {
+		num = rand() % 10;
+		counts[num]++;
+	}
+	for (int i = 0; i < 10; i++)
+		cout << setw(3) << i;
+	cout << "\n-------------------------------\n";
+	for (int i = 0; i < 10; i++)
+		cout << setw(3) << counts[i];
+
+	return 0;
+}
