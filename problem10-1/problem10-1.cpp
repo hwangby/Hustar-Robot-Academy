9995f5650847cdb59d1e5c804ecc0388a9a4c9c8
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-08 15:55:54 +0900

    Add files via upload

---
+++ problem10-1/problem10-1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	cout << "Enter a string s1: ";
+	cin >> s1;
+	cout << "Enter a string s2: ";
+	cin >> s2;
+	string sortedS1 = s1, sortedS2 = s2;
+	sort(sortedS1.begin(), sortedS1.end());
+	sort(sortedS2.begin(), sortedS2.end());
+	
+	if (sortedS1 == sortedS2) {
+		cout << s1 << " and " << s2 << " are anagrams";
+	}
+	else {
+		cout << s1 << " and " << s2 << " are not anagrams";
+		return 0;
+	}
+	
+}
