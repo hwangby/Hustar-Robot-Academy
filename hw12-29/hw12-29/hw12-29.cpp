82f0021380adef6b3d51525f6184c010c905cb17
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-11 00:23:52 +0900

    Add files via upload

---
+++ hw12-29/hw12-29/hw12-29.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter number n: ";
+	cin >> n;
+
+	cout << "Enter " << n << " rows of letters separated by spaces:\n";
+	vector<vector<char>> latinSquare(n);
+	vector<char> row(n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			char element;
+			cin >> element;
+			if (element - 'A' >= n) {
+				cout << "Wrong input: the letters must be from A to " << char('A' + n-1) << endl;
+				return 0;
+			}
+			row[j] = element;
+		}
+		latinSquare[i] = row;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = j+1; k < n; k++) {
+				if (latinSquare[i][j] == latinSquare[i][k] || latinSquare[j][i] == latinSquare[k][i]) {
+					cout << "The input array is NOT a Latin square\n";
+					return 0;
+				}
+			}
+		}
+	}
+	
+	cout << "The input array is a Latin square" << endl;
+
+	return 0; 
+}
