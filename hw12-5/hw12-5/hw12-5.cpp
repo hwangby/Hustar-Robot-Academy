f7c8988f2c12d5124999b0c4a82904c8d595af5c
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-10 22:43:08 +0900

    Add files via upload

---
+++ hw12-5/hw12-5/hw12-5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+void tswap(T &value1, T &value2) {
+	T temp = value1;
+	value1 = value2;
+	value2 = temp;
+}
+
+int main()
+{
+	int intA = 1, intB = 2;
+	tswap(intA, intB);
+	cout << intA << " " << intB << endl;
+
+	double doubleA = 1.1, doubleB = 2.2;
+	tswap(doubleA, doubleB);
+	cout << doubleA << " " << doubleB << endl;
+
+	string stringA = "abc", stringB = "def";
+	tswap(stringA, stringB);
+	cout << stringA << " " << stringB;
+}
