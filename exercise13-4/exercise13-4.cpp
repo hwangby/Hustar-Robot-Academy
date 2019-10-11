8edb894d6f8cadc3e1631eaa5a881db5b5b7205e
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-11 15:01:15 +0900

    Add files via upload

---
+++ exercise13-4/exercise13-4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	fstream output("Exercise13_4.txt", ios::out);
+	for (int i = 0; i < 100; i++) {
+		output << rand() % 100 << ' ';
+	}
+	output.close();
+
+	fstream input("Exercise13_4.txt", ios::in | ios::out);
+	vector<int> v;
+	while (!input.eof()) {
+		int num;
+		input >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	input.clear();
+	input.seekg(0, ios::beg);
+	for (int i = 0; i < 100; i++) {
+		input << v[i] << ' ';
+	}
+	input.close();
+
+	cout << "Done";
+
+	return 0;
+}
