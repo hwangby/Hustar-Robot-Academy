ec68d6229db90c5aff1fddb1da663efbfbd50be9
hwangby <53309552+hwangby@users.noreply.github.com>
2019-10-11 13:33:09 +0900

    Add files via upload

---
+++ exercise13-1/exercise13-1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	fstream output;
+	fstream input;
+	input.open("Exercise13_1.txt", ios::in);
+
+	if (input.fail()) {
+		output.open("Exercise13_1.txt", ios::out);
+		for (int i = 0; i < 100; i++)
+			output << i << " ";
+		output.close();
+		cout << "file creation\n";
+	}
+	else {
+		output.open("Exercise13_1.txt", ios::out | ios::app);
+		for (int i = 100; i > 0; i--)
+			output << i << " ";
+		cout << "writing number\n";
+		output.close();
+		input.close();
+	}
+	return 0;
+}
